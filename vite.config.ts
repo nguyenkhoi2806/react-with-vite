630327d89e4bf99cdf4930c28771200b5d104edb
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2022-12-02 15:49:16 +0700

    remove lib not use

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
